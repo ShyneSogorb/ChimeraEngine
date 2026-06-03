584bdbcd4383a406ed90d225c489740e25ed6e1d
Shyne <50318883+Docmil003@users.noreply.github.com>
2026-06-03 15:53:45 +0200

    Refactor project structure, replace FetchContent with find_package, add CMake presets, and update TMap implementation

--- Engine/ChimeraCore/Source/Public/Containers/Map.h
+++ Engine/ChimeraCore/Source/Public/Containers/Map.h
@@ -1,8 +1,27 @@
 ﻿
 #pragma once
 #include <unordered_map>
+#include <EASTL/unordered_map.h>
 
 
-template <typename Key, typename Value> 
-using TMap = std::unordered_map<Key, Value>;
-
+template <typename KeyType, typename ValueType> 
+struct TMap
+{
+    TMap() = default;
+    
+    template <typename... Args>
+    void Emplace(Args&&... Arguments)
+    {
+        Map.emplace(std::forward<Args>(Arguments)...);
+    }
+    
+    ValueType& operator[](const KeyType& Key) { return Map[Key]; }
+    
+    const ValueType& operator[](const KeyType& Key) const { return Map.at(Key); }
+    
+    size_t Num() const { return Map.size(); }
+    
+private:
+    
+    eastl::unordered_map<KeyType, ValueType> Map;
+};
